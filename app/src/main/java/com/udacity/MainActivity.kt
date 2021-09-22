44f0f19eb1aee511ea395c67ca874d4ae6985556
MariemMezghani <35550711+MariemMezghani@users.noreply.github.com>
2021-09-22 10:33:42 +0200

    download different repositories in a phone file

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -12,16 +12,25 @@ import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import com.udacity.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     private var downloadID: Long = 0
+    private var fileName: String = ""
+    private var selectedURL = ""
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
@@ -29,7 +38,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+
         setSupportActionBar(toolbar)
         // notificationManager instance
         notificationManager = ContextCompat.getSystemService(
@@ -41,10 +52,31 @@ class MainActivity : AppCompatActivity() {
         // register the BroadcastReciever
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
-        custom_button.setOnClickListener {
-            download()
+        binding.content.customButton.setOnClickListener {
+            when (binding.content.radioGroup.checkedRadioButtonId) {
+                View.NO_ID ->
+                    Toast.makeText(
+                            this@MainActivity,
+                            "Please select the file to download",
+                            Toast.LENGTH_SHORT).show()
+                else -> {
+                    fileName = findViewById<RadioButton>(binding.content.radioGroup.checkedRadioButtonId)
+                            .text.toString()
+                    if (fileName == getString(R.string.loadApp_download)) {
+                        selectedURL = getString(R.string.loadApp_download_link)
+                    } else if (fileName == getString(R.string.glide_download)) {
+                        selectedURL = getString(R.string.glide_download_link)
+                    } else {
+                        selectedURL = getString(R.string.retrofit_download_link)
+                    }
+
+                    download()
+                }
+            }
 
         }
+        setContentView(binding.root)
+
     }
 
     // BroadcastReciever instance
@@ -53,23 +85,35 @@ class MainActivity : AppCompatActivity() {
             // val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
             // Reset download state
             custom_button.buttonState = ButtonState.Completed
-            notificationManager.sendNotification("file download is complete", applicationContext)
+            notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+            custom_button.buttonState = ButtonState.Completed
+            notificationManager.sendNotification(fileName, applicationContext)
+
+
         }
     }
 
     private fun download() {
         custom_button.buttonState = ButtonState.Loading
+        // Make a directory
+        val direct = File(getExternalFilesDir(null), "/repos")
+        if (!direct.exists()) {
+            direct.mkdirs()
+        }
         val request =
-                DownloadManager.Request(Uri.parse(URL))
+                DownloadManager.Request(Uri.parse(selectedURL))
                         .setTitle(getString(R.string.app_name))
                         .setDescription(getString(R.string.app_description))
                         .setRequiresCharging(false)
                         .setAllowedOverMetered(true)
                         .setAllowedOverRoaming(true)
+                        .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "/repositories/repository.zip")
+
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
                 downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+
     }
     // create notificationChannel
 
@@ -89,6 +133,7 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
 
     companion object {
         private const val URL =
